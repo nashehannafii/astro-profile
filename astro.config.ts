1ffaa14adf459d13aa85aa20bb9c085bf4152554
nashehannafii <nashehannafii@gmail.com>
2024-06-11 23:45:58 +0700

    update

--- astro.config.ts
+++ astro.config.ts
@@ -5,13 +5,10 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-// import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,  
-  // output: 'server',
-  // adapter: vercel(),
+  site: SITE.website,
   integrations: [
     tailwind({
       applyBaseStyles: false,
